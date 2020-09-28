6a24ed7c5ef91cdc20b7b5e86e89a6b760e50473
Felipe Tourinho <70729584+felipe-mct@users.noreply.github.com>
2020-09-28 19:05:30 -0300

    funções offset gyro e accel
    -implementadas funções que retornam os valores de offset do giroscópio e do acelerômetro

--- DAQ/libdac.h
+++ DAQ/libdac.h
@@ -56,3 +56,69 @@ void confMag (uint8_t * buf, uint8_t M1, uint8_t M2, uint8_t M3)
 	/*??*/	e2prom[i] = (int16_t)(((int16_t)buf[j] << 8)  | buf[k]) ;//Turn the MSB and LSB into a signed 16-bit value
 	}
     }
+
+
+ float offsetAccel (char s)
+ {
+	buf[0] = XA_OFFSET_H ;
+	ret = HAL_I2C_Master_Transmit(&hi2c1,MPU9250_AD,buf,1,100) ;
+	if (ret != HAL_OK){
+		strcpy((char*)msg,"Error Tx\r\n") ;
+	}
+	else{
+		ret = HAL_I2C_Master_Receive(&hi2c1,MPU9250_AD,buf,6,100) ;
+		if (ret != HAL_OK){
+			strcpy((char*)msg,"Error Rx\r\n") ;
+		}
+		else{
+			data[0] = (int16_t)(((int16_t)buf[0] << 8)  | buf[1]) ;  // Turn the MSB and LSB into a signed 16-bit value
+			data[1] = (int16_t)(((int16_t)buf[2] << 8)  | buf[3]) ;
+			data[2] = (int16_t)(((int16_t)buf[4] << 8)  | buf[5]) ;
+			
+			ax_off = data[0] * aRes;
+			ay_off = data[0] * aRes;
+			az_off = data[0] * aRes;
+			
+			sprintf((char*)msg,"ax_off=%f  ay_off=%f  az_off=%f  [degrees/s]", ax_off,ay_off,az_off);
+		}
+	}
+	 switch (s) {
+	 	case x: return ax_off;
+		case y: return ay_off;
+		case z: return az_off;
+		default: return 01100101 01110010 01110010 01101111;
+	 }
+ }
+
+ float offsetGyro (char s)
+ {
+	buf[0] = XG_OFFSET_H ;
+	ret = HAL_I2C_Master_Transmit(&hi2c1,MPU9250_AD,buf,1,100) ;
+	if (ret != HAL_OK){
+		strcpy((char*)msg,"Error Tx\r\n") ;
+	}
+	else{
+		ret = HAL_I2C_Master_Receive(&hi2c1,MPU9250_AD,buf,6,100) ;
+		if (ret != HAL_OK){
+			strcpy((char*)msg,"Error Rx\r\n") ;
+		}
+		else{
+			data[0] = (int16_t)(((int16_t)buf[0] << 8)  | buf[1]) ;  // Turn the MSB and LSB into a signed 16-bit value
+			data[1] = (int16_t)(((int16_t)buf[2] << 8)  | buf[3]) ;
+			data[2] = (int16_t)(((int16_t)buf[4] << 8)  | buf[5]) ;
+			
+			gx_off = data[0] * gRes;
+			gy_off = data[0] * gRes;
+			gz_off = data[0] * gRes;
+			
+			sprintf((char*)msg,"gx_off=%f  gy_off=%f  gz_off=%f  [degrees/s]", gx_off,gy_off,gz_off);
+		}
+	}
+	 switch (s) {
+	 	case x: return gx_off;
+		case y: return gy_off;
+		case z: return gz_off;
+		default: return 01100101 01110010 01110010 01101111;
+	 }
+ }
+	 
