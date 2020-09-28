d48f4ccd04cfbc0626615c5ff1a736fc16cc2637
laurasrs <70729015+laurasrs@users.noreply.github.com>
2020-09-28 20:16:25 -0300

    Update libdac.h

--- DAQ/libdac.h
+++ DAQ/libdac.h
@@ -1,12 +1,15 @@
-//inicialização da biblioteca a fazer
+//inicialização da biblioteca 
+#include "string.h"
+#include "stdio.h"
+
+//Variaveis globais
+long int B5;
 
 
 
+//Funçoes
 
-
-
-//funçoes
-
+//Configuraçao: Acel, Giro, Mag, Bmp
 void confgAce (uint8_t * buf, uint8_t A1)
 {
 	buf[0] = A1; //ACE_CONFIG_AD
@@ -57,7 +60,7 @@ void confMag (uint8_t * buf, uint8_t M1, uint8_t M2, uint8_t M3)
 	}
     }
 
-
+//Offset : Accel a Giro
  float offsetAccel (char s)
  {
 	buf[0] = XA_OFFSET_H ;
@@ -122,33 +125,47 @@ void confMag (uint8_t * buf, uint8_t M1, uint8_t M2, uint8_t M3)
 	 }
  }
 
- float adjMagnet (char s) //essa função só retorna o fator que multiplicado pela leitura do magnetômetro resulta no adjustment
- {
-	buf[0] = MAG_ASAX ;
-	ret = HAL_I2C_Master_Transmit(&hi2c1,MPU9250_AD,buf,1,100) ;
-	if (ret != HAL_OK){
-		strcpy((char*)msg,"Error Tx\r\n") ;
-	}
-	else{
-		ret = HAL_I2C_Master_Receive(&hi2c1,MPU9250_AD,buf,6,100) ;
-		if (ret != HAL_OK){
-			strcpy((char*)msg,"Error Rx\r\n") ;
-		}
-		else{
-			data[0] = (float) buf[0] ; 
-			data[1] = (float) buf[2] ;
-			data[2] = (float) buf[4] ;
-			
-			mx_adj = (float) (data[0]-128)/256 + 1.
-			my_adj = (float) (data[1]-128)/256 + 1. 
-			mz_adj = (float) (data[2]-128)/256 + 1.
+//Calculo da temperatura e pressao
 
-			sprintf((char*)msg,"mx_adj=%f*Hx   my_adj=%f*Hy  mz_adj=%f*Hz\r\n",mx_adj,my_adj,mz_adj);
-		}
+float getTemp(uint8_t * buf, uint16_t * e2prom) //vetor e2prom calculado na confgBmp
+{
+	float Temp = 0;
+	long int UT, X1, X2;
+	B5 = 0; //Variavel global
+	
+	UT = (int16_t)(((int16_t)buf[0] << 8)  | buf[1]) ; //Turn the MSB and LSB into a signed 16-bit value
+	X1 = (UT-e2prom[5])*e2prom[4]/(2^15);//32768
+	X2 = e2prom[9]*2^11/(X1+e2prom[10]);
+	B5 = X1+X2; 
+	Temp = ((B5+8)/(2^4))/10;
+	
+	return Temp;
+}
+
+long int getPressure(uint8_t * buf, uint16_t * e2prom)
+{
+	long int UP, X1, X2, X3, B3, B4, B6, B7, P;
+	
+	UP = (int16_t)(((int16_t)buf[0] << 16)  | buf[1] << 8 | buf[2]) >> (8-2); //Junta o MSB, o LSB e o XLSB em um único número de 24 bits
+	B6 = B5-4000;  //Variavel global - B5
+	X1 = (e2prom[7]*(B6*B6/2^12))/(2^11);
+	X2 = e2prom[1]*B6/(2^11);
+	X3 = X1 + X2;
+	B3 = (((e2prom[0]*4+X3)<<2)+2)/4;
+	X1 = e2prom[2]*B6/(2^13);
+	X2 = (e2prom[6]*(B6*B6/2^12))/(2^16);
+	X3 = ((X1+X2)+2)/4;
+	B4 = e2prom[3]*(unsigned long)(X3+32768)/(2^15);
+	B7 = ((unsigned long)UP-B3)*(50000>>2);
+	if(B7<0x80000000){
+		P = (B7*2)/B4;
+	}else{
+		P = (B7/B4)*2;
 	}
-	case x: return mx_adj;
-	case y: return my_adj;
-	case z: return mz_adj;
-	default: return 01100101 01110010 01110010 01101111;
- }
-	 
+	X1 = (P/2^8)*(P/2^8);
+	X1 = (X1*3038)/(2^16);
+	X2 = (-7357*P)/(2^16);
+	P = P+(X1+X2+3791)/(2^4);
+	
+	return P;
+}
