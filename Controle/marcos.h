3f2dabbdb2ff87ef99dab99f7a98fe26547eb66e
AuroraMaciel <50123198+AuroraMaciel@users.noreply.github.com>
2021-03-15 20:06:59 -0300

    Update marcos.h

--- Controle/marcos.h
+++ Controle/marcos.h
@@ -3,6 +3,12 @@
 
 float targetObj[3];//x, y, z
 
+/*  dados vindos da DAQ */
+float gps_xyz[3]; //posição
+float drone_pos[3];//orientacao + pos (?)
+float angVel[3];
+/*  fim dados vindos da DAQ */
+
 float particlesTargetVelocities[4] = {0, 0, 0, 0}; //força do motor
 
 // Constantes de PID //
@@ -42,14 +48,6 @@ float p0=1, p1=0, p2=0, p3=0; //quaternion ref, r,p,y zero
 
 float Ts;
 
-/*  dados vindos da DAQ */
-float gps_xyz[3];
-float drone_pos[3];//orientacao + pos (?)
-float angVel[3];
-/*  fim dados vindos da DAQ */
-
-void P2Controller();
-
 float *getObjectPosition(float *veta, float *vetb) //retorna o erro de posição entre o drone e o target
 {
 	float *vetc = (float*)malloc(3*sizeof(float));
@@ -70,6 +68,39 @@ float *getObjectOrientation(float *veta, float *vetb) //retorna o erro de orient
 	}
 	
 	return vetc;
+}
+
+void P2Controller()
+{
+    //~ -- Full Quaternion Based Attitude Control for a Quadrotor, Emil Fresk and George Nikolakopoulos,
+    //~ -- European Control Conference, 2013
+    float q0 = qs[0];
+    float q1 = qs[1];
+    float q2 = qs[2];
+    float q3 = qs[3];
+    float qErr[3] = {0,0,0};
+    
+    q1 = -q1;
+    q2 = -q2;
+    q3 = -q3;
+    //~ -- Product: q_ref (x) *q_m, eq. 19:
+    float qErr0 = p0*q0 - p1*q1 - p2*q2 - p3*q3;
+    qErr[0] = p0*q1 + p1*q0 + p2*q3 - p3*q2;
+    qErr[1] = p0*q2 - p1*q3 + p2*q0 + p3*q1;
+    qErr[2] = p0*q3 + p1*q2 - p2*q1 + p3*q0;
+    //~ -- eq. 20:
+    if (qErr0 < 0)
+    {
+        qErr[0] = -qErr[0];
+        qErr[1] = -qErr[1];
+        qErr[2] = -qErr[2];
+    }
+    //~ -- eq. 21:
+    for(int i=0; i<3; i++)
+    {
+        p2tau[i] = -KwP2Controller*angVel[i] - KqP2Controller*qErr[i];
+    }
+
 }
 
 #endif
