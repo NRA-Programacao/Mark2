fdb7f5decb40802046bab02e65757ce79da2dcc2
AuroraMaciel <50123198+AuroraMaciel@users.noreply.github.com>
2021-03-15 19:51:11 -0300

    Update marcos.h

--- Controle/marcos.h
+++ Controle/marcos.h
@@ -2,13 +2,23 @@
 #define MARCOS_BIB
 
 float targetObj[3];//x, y, z
-float particlesTargetVelocities[4] = {0, 0, 0, 0};
 
+float particlesTargetVelocities[4] = {0, 0, 0, 0}; //força do motor
+
+// Constantes de PID //
 int pParam=2;
 int iParam=0;
 int dParam=0;
 int vParam=-2;
+//------------------//
+float attKp = 0.25;
+float attKd = 2.10;
+float posKp = 0.005;
+float posKd = 1.000;
+float posKi = 0.003;
+// Fim das constantes de PID //
 
+//Variaveis de erro e integradores//
 float cumul=0;
 float lastE=0;
 float pAlphaE=0;
@@ -21,17 +31,12 @@ float yaw_error_last = 0;
 
 float integradorX = 0;
 float integradorY = 0;
+//Fim das variaveis de erro e integradores//
 
 float KqP2Controller = 2.05;
 float KwP2Controller = 0.4;
 float p2tau[3] = {0,0,0};
-float qs[4] = {1,0,0,0};
-
-float attKp = 0.25;
-float attKd = 2.10;
-float posKp = 0.005;
-float posKd = 1.000;
-float posKi = 0.003;
+float qs[4] = {1,0,0,0}; //quaternion de referencia
 
 float p0=1, p1=0, p2=0, p3=0; //quaternion ref, r,p,y zero
 
@@ -45,7 +50,7 @@ float angVel[3];
 
 void P2Controller();
 
-float *getObjectPosition(float *veta, float *vetb)
+float *getObjectPosition(float *veta, float *vetb) //retorna o erro de posição entre o drone e o target
 {
 	float *vetc = (float*)malloc(3*sizeof(float));
 	for(int i=0; i<3; i++)
@@ -56,7 +61,7 @@ float *getObjectPosition(float *veta, float *vetb)
 	return vetc;
 }
 
-float *getObjectOrientation(float *veta, float *vetb)
+float *getObjectOrientation(float *veta, float *vetb) //retorna o erro de orientação entre o drone e o target
 {
 	float *vetc = (float*)malloc(3*sizeof(float));
 	for(int i=0; i<3; i++)
