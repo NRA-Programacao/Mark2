765b9e9259e68a971e7d735f1158ca8a2971ff0f
AuroraMaciel <50123198+AuroraMaciel@users.noreply.github.com>
2021-03-15 19:46:30 -0300

    Add files via upload

---
+++ Controle/marcos.h
@@ -0,0 +1,70 @@
+#ifndef MARCOS_BIB
+#define MARCOS_BIB
+
+float targetObj[3];//x, y, z
+float particlesTargetVelocities[4] = {0, 0, 0, 0};
+
+int pParam=2;
+int iParam=0;
+int dParam=0;
+int vParam=-2;
+
+float cumul=0;
+float lastE=0;
+float pAlphaE=0;
+float pBetaE=0;
+float posErrorY_last=0;
+float posErrorX_last=0;
+
+float yaw_quad = 0;
+float yaw_error_last = 0;
+
+float integradorX = 0;
+float integradorY = 0;
+
+float KqP2Controller = 2.05;
+float KwP2Controller = 0.4;
+float p2tau[3] = {0,0,0};
+float qs[4] = {1,0,0,0};
+
+float attKp = 0.25;
+float attKd = 2.10;
+float posKp = 0.005;
+float posKd = 1.000;
+float posKi = 0.003;
+
+float p0=1, p1=0, p2=0, p3=0; //quaternion ref, r,p,y zero
+
+float Ts;
+
+/*  dados vindos da DAQ */
+float gps_xyz[3];
+float drone_pos[3];//orientacao + pos (?)
+float angVel[3];
+/*  fim dados vindos da DAQ */
+
+void P2Controller();
+
+float *getObjectPosition(float *veta, float *vetb)
+{
+	float *vetc = (float*)malloc(3*sizeof(float));
+	for(int i=0; i<3; i++)
+	{
+		vetc[i] = veta[i]-vetb[i];
+	}
+	
+	return vetc;
+}
+
+float *getObjectOrientation(float *veta, float *vetb)
+{
+	float *vetc = (float*)malloc(3*sizeof(float));
+	for(int i=0; i<3; i++)
+	{
+		vetc[i] = veta[i]-vetb[i];
+	}
+	
+	return vetc;
+}
+
+#endif
