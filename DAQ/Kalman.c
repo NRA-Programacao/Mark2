4d62befae29500ffd76a29c65c279ffd9c73ab0c
Felipe Tourinho <70729584+felipe-mct@users.noreply.github.com>
2021-03-22 21:56:56 -0300

    Update Kalman.c
    - equações de predição
    - matrizes de medida
    - a fazer: matrizes de covariância

--- DAQ/Kalman.c
+++ DAQ/Kalman.c
@@ -4,26 +4,30 @@
 double t; //intervalo de tempo do filtro de Kalman
 int alfa = 1e-3, ki = 0, beta = 2;
 
-double X_m0 = {0 0 0 0 0 0 0 0 0 0 0 0}; //estados medidos k-1
-double X_m1 = {0 0 0 0 0 0 0 0 0 0 0 0}; //estados medidos k
-double a = [[ax], [ay], [az]]; //sensores
-double g = [gx gy gz]; 
-double m = [mx my mz]; 
-double R = [[cos(mx)cos(mz) sin(mx)sin(my)cos(mz)-cos(mx)sin(mz) cos(mx)sin(my)cos(mz)+sin(mx)sin(mz)] [sin(my)sin(mz) sin(mx)sin(my)sin(mz)+cos(mx)cos(mz) cos(mx)sin(my)sin(mz)-sin(mx)cos(mz)] [-sin(my) sin(mx)cos(my) cos(mx)cos(my)]]; //Earth-fixed to body frame
-double gr = [0 0 -9.81]; //vetor gravidade 
-double M = [[t*t/2 0 0], [0 t*t/2 0], [0 0 t*t/2], [0 0 0], [0 0 0], [0 0 0], [0 0 0], [0 0 0], [0 0 0], [0 0 0], [0 0 0], [0 0 0]]; //matriz acelerômetro
-double N = [[t 0 0] [0 t 0] [0 0 t]]; //matriz giroscópio
+double X_m0[12][1] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}}; //estados medidos k-1
+double X_m1[12][1] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}}; //estados medidos k
+double a[3][1] = {{ax}, {ay}, {az}};  //acelerômetro
+double g[3][1] = {{gx}, {gy}, {gz}};  //giroscópio
+double m[3][1] = {{mx}, {my}, {mz},}; //magnetômetro
+double R[3][3] = {{cos(mx)cos(mz), sin(mx)sin(my)cos(mz)-cos(mx)sin(mz), cos(mx)sin(my)cos(mz)+sin(mx)sin(mz)}, {sin(my)sin(mz), sin(mx)sin(my)sin(mz)+cos(mx)cos(mz), cos(mx)sin(my)sin(mz)-sin(mx)cos(mz)}, {-sin(my), sin(mx)cos(my), cos(mx)cos(my)}}; //Earth-fixed to body frame
+double gr[3][1] = {{0}, {0}, {-9.81}}; //vetor gravidade 
+double M[12][3] = {{t*t/2, 0, 0}, {0, t*t/2, 0}, {0, 0, t*t/2}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {t, 0, 0}, {0, t, 0}, {0, 0, t}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; //matriz acelerômetro
+double N[12][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {t, 0, 0}, {0, t, 0}, {0, 0, t}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}}; //matriz giroscópio
             
-double X = [x y z r p ya vx vy vz vr vp vya] = [0 0 0 0 0 0 0 0 0 0 0 0]; //vetor de estados
-double A = [[1 0 0 0 0 0 t 0 0 0 0 0], [0 1 0 0 0 0 0 t 0 0 0 0], [0 0 1 0 0 0 0 0 t 0 0 0], [0 0 0 1 0 0 0 0 0 t 0 0 ], [0 0 0 0 1 0 0 0 0 0 t 0], [0 0 0 0 0 1 0 0 0 0 0 t], [0 0 0 0 0 0 1 0 0 0 0 0], [0 0 0 0 0 0 0 1 0 0 0 0], [0 0 0 0 0 0 0 0 1 0 0 0], [0 0 0 0 0 0 0 0 0 1 0 0], [0 0 0 0 0 0 0 0 0 0 1 0], [0 0 0 0 0 0 0 0 0 0 0 1]]; //matriz de estados
-double u = [accx accy accz accr accp accya]; //vetor de controle
-double B = [[t*t/2 0 0 0 0 0], [0 t*t/2 0 0 0 0], [0 0 t*t/2 0 0 0], [0 0 0 t*t/2 0 0], [0 0 0 0 t*t/2 0], [0 0 0 0 0 t*t/2], [t 0 0 0 0 0], [0 t 0 0 0 0], [0 0 t 0 0 0], [0 0 0 t 0 0], [0 0 0 0 t 0], [0 0 0 0 0 t]]; //matriz de controle
+double X[12][1] = {{x}, {y}, {z}, {r}, {p}, {ya}, {vx}, {vy}, {vz}, {vr}, {vp}, {vya}} = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}}; //vetor de estados
+double A[12][12] = {{1, 0, 0, 0, 0, 0, t, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, t, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0, t, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, t, 0, 0,}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, t, 0,}, {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, t}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}}; //matriz de estados
+double u[6][1] = {{accx}, {accy}, {accz}, {accr}, {accp}, {accya}}; //vetor de controle
+double B[12][6] = {{t*t/2 0 0 0 0 0}, {0 t*t/2 0 0 0 0}, {0 0 t*t/2 0 0 0}, {0 0 0 t*t/2 0 0}, {0 0 0 0 t*t/2 0}, {0 0 0 0 0 t*t/2}, {t 0 0 0 0 0}, {0 t 0 0 0 0}, {0 0 t 0 0 0}, {0 0 0 t 0 0}, {0 0 0 0 t 0}, {0 0 0 0 0 t}}; //matriz de controle
+
+double P[12][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; //matriz de covariância
+
 
 int main ()
 {
  X = X*A + B*u;
  a = R*(a-gr);
- M_m1 = X_m0 + M*a
+ X_m1 = X_m0 + M*a + N*g;
+ 
   
 }
 /*
