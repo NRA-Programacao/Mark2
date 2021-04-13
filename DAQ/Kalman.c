b87aebabdfc3056aba2f399af9727cee87548ce8
Felipe Tourinho <70729584+felipe-mct@users.noreply.github.com>
2021-04-12 21:34:55 -0300

    Update Kalman.c
    - adicionado código da decomposição de Cholesky
    - adicionado código para seleção dos pontos sigma
    - adicionado código para pesos dos pontos sigma
    - adicionado rascunho de código para cálculo da média durante prediction step

--- DAQ/Kalman.c
+++ DAQ/Kalman.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 double t; //intervalo de tempo do filtro de Kalman
-int alfa = 1e-3, ki = 0, beta = 2;
+int alfa = 1e-3, ki = 0, beta = 2, n = 12, i, j;
+double mu = 0;
 
 double X_m0[12][1] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}}; //estados medidos k
 double X_m1[12][1] = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}}; //estados medidos k-1
@@ -21,8 +25,39 @@ double X[12][1] = {{x}, {y}, {z}, {r}, {p}, {ya}, {vx}, {vy}, {vz}, {vr}, {vp}, 
 double A[12][12] = {{1, 0, 0, 0, 0, 0, t, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, t, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0, t, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, t, 0, 0,}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, t, 0,}, {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, t}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}}; //matriz de estados
 double u[6][1] = {{accx}, {accy}, {accz}, {accr}, {accp}, {accya}}; //vetor de controle
 double B[12][6] = {{t*t/2 0 0 0 0 0}, {0 t*t/2 0 0 0 0}, {0 0 t*t/2 0 0 0}, {0 0 0 t*t/2 0 0}, {0 0 0 0 t*t/2 0}, {0 0 0 0 0 t*t/2}, {t 0 0 0 0 0}, {0 t 0 0 0 0}, {0 0 t 0 0 0}, {0 0 0 t 0 0}, {0 0 0 0 t 0}, {0 0 0 0 0 t}}; //matriz de controle
+double controle[12][1];
 
-double P[12][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; //matriz de covariância
+double P[12][12] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; //matriz de covariância
+double P_[12][12]= {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; //matriz "covariância" para pontos sigma
+
+double lower[12][12]; //para decomposição de Cholesky
+
+double sigma[25][12]; //matriz pontos Sigma
+
+double weight_m[25][1];
+double weight_c[25][1];
 
 double media(double _0[12][1], double _1[12][1], double _2[12][1], double _3[12][1], double _4[12][1])
 {
@@ -83,15 +118,123 @@ double covariancia(double media[12][1], double _0[12][1], double _1[12][1], doub
             return covar;
 }
 
+void Cholesky_Decomposition(double matrix[12][12])
+{
+    // Decomposing a matrix into Lower Triangular
+    for (int i = 0; i < 12; i++) 
+    {
+        for (int j = 0; j <= i; j++) 
+        {
+            double sum = 0;
+ 
+            if (j == i) // summation for diagnols
+            {
+                for (int k = 0; k < j; k++)
+                    sum += pow(lower[j][k], 2);
+                lower[j][j] = sqrt(matrix[j][j] - sum);
+            } 
+            else 
+            {
+                // Evaluating L(i, j) using L(j, j)
+                for (int k = 0; k < j; k++)
+                    sum += (lower[i][k] * lower[j][k]);
+                lower[i][j] = (matrix[i][j] - sum) / lower[j][j];
+            }
+        }
+    }
+}
 
 
 int main ()
 {
-            X = X*A + B*u;
+           // X = X*A + B*u;
             a = R*(a-gr);
             X_m1 = X_m0 + M*a + N*g;
-
+            
+            //Pontos Sigma
+            
+            lambda = alfa*alfa*(n+ki)-n;
+            P_ = (n+lamba)*P;
+            Cholesky_Decomposition(P_);
+            
+            for (i=0, i<12, i++)
+            {
+                        sigma[i][0] = mu;
+            }
+            
+            for (i=1, i<13, i++)
+            {
+                        for (j=0, j<12, j++)
+                        {
+                                    sigma[j][i] = mu + lower[j][i];
+                                    sigma[j][i+12] = mu - lower[j][i];
+                        }
+            }
+            
+            //Pesos dos pontos
+            
+            weight_m[0][0] = lambda/(n+lambda);
+            weight_c[0][0] = weight_m[0][0] + (1 + alfa*alfa + beta);  
+            
+            for (i=1, i<25, i++)
+            {
+                        weight_m[i][0] = 1/2*(n+lambda);
+                        weight_c[i][0] = weight_m[i][0];
+            }
 }
+
+
+/*Prediction Step - deve ser integrado à int main()
+
+
+//f(sigma, u) = deve ser transformado em função
+//B*u
+for (i=0, i<12, i++)
+{
+            sum=0;
+            for (j=0, j<6, j++)
+            {
+                        sum = sum + B[i][j]*u[j][0];
+            }
+            controle[i][0] = sum;
+ }
+            
+//A*sigma
+for (i=0, i<12, i++)
+{
+            for (j=0, j<25, j++)
+            {
+                        sum = 0;
+                        for (k=0, k<12, k++)
+                        {
+                                    sum = sum + A[i][k]*sigma[k][j];
+                        }
+                        estado[i][j] = sum; 
+             }
+ }
+ 
+//A*sigma + B*u
+for (i=0, i<12, i++)
+{
+            for (j=0, j<25, j++)
+            {
+                        estado[i][j] = estado[i][j] + controle[i][0];
+            }
+ }
+
+//cálculo média
+for (i=0, i<25, i++)
+{
+            for (j=0, j<12, j++)
+            {
+                        mu = mu + weight_m[i][0]*estado[j][i];
+            }
+ }
+ 
+ 
+
+
+
 /*
 roll_quad  = math.atan(2*(q2*q3 + q0*q1), q0^2 - q1^2 - q2^2 - q3^2) -- Roll
 pitch_quad = math.asin(-2*(q1*q3 - q0*q2))                           -- Pitch
