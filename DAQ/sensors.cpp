c9a3d982a8d8fbe379a6b8d2dfd646fa30782ee7
AuroraMaciel <50123198+AuroraMaciel@users.noreply.github.com>
2021-06-15 18:59:53 -0300

    Update sensors.cpp

--- DAQ/sensors.cpp
+++ DAQ/sensors.cpp
@@ -17,7 +17,8 @@ class sensores
 {
 public:
   double z; // altura do drone baseado no sensor ultrassonico
-  double mx, my, mz; //resposta do magnetometro 
+  double mx, my, mz; //resposta do magnetometro
+  double yaw //yaw vindo da visão (line follower)
 
 void callback_sonar(const sensor_msgs::Range &h)
   {
@@ -29,22 +30,26 @@ void callback_magnetic(const geometry_msgs::Vector3 &angulo)
     my = angulo.x;
     mz = angulo.z;
   }
+ void callback_linefollower(const geometry_msgs::Pose2D &drone_pose)
+  {
+    yaw = drone_pose.theta;
+  }
 };
 
 int main(int argc, char **argv)
 {
   
   // Init ROS node
-  controle vars;
+  sensores vars;
 
-  ros::init(argc, argv, "sensors");
-  ros::Publisher pub_vel = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
-  ros::Publisher pub_centralizeQRcode = nh.advertise<std_msgs::Bool>("controle/centralizeQRcode", 10);
-  ros::Subscriber sub_sonar = nh.subscribe("/sonar_height", 10, &controle::callback_sonar, &vars);
-  ros::Subscriber sub_mag = nh.subscribe("/magnetic", 10, &controle::callback_magnetic, &vars);
+  ros::init(argc, argv, "sensores");
+//  ros::Publisher pub_vel = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+//  ros::Publisher pub_centralizeQRcode = nh.advertise<std_msgs::Bool>("controle/centralizeQRcode", 10);
+  ros::Subscriber sub_sonar = nh.subscribe("/sonar_height", 10, &sensores::callback_sonar, &vars);
+  ros::Subscriber sub_mag = nh.subscribe("/magnetic", 10, &sensores::callback_magnetic, &vars);
   ros::Subscriber sub_linefollower = nh.subscribe("/line_follower", 10, &controle::callback_linefollower, &vars);
-  ros::Subscriber sub_qrcode = nh.subscribe("/qrcode/posicao", 10, &controle::callback_QRcode, &vars);
-  ros::Subscriber sub_vel = nh.subscribe("cmd_vel", 10, &controle::callback_twist, &vars);
+//  ros::Subscriber sub_qrcode = nh.subscribe("/qrcode/posicao", 10, &controle::callback_QRcode, &vars);
+//  ros::Subscriber sub_vel = nh.subscribe("cmd_vel", 10, &controle::callback_twist, &vars);
   ros::Rate loop_rate(1000);
 
   geometry_msgs::Twist twist;
